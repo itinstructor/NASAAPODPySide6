fa32dbedf83c8d1ad79ccae823b1b4ad0a4ec753
William Loring <32806514+itinstructor@users.noreply.github.com>
2024-12-03 06:34:36 -0700

    commit message

---
+++ NASA APOD V4/apod_program.py
@@ -0,0 +1,280 @@
+"""
+    Name: apod_program.py
+    Author: William A Loring
+    Created: 09-29-2022
+    Purpose: Get Astronomy Photo of the Day
+    Display as a thumbnail and normal size
+    Command line to rebuild ui to py
+    pyside6-uic main_window.ui -o ui_main_window.py
+"""
+
+import sys
+import datetime
+import PySide6
+# pip install pyside6
+from PySide6.QtCore import Qt, QTimer, QDate
+from PySide6.QtGui import QIcon, QPixmap, QGuiApplication
+from PySide6.QtWidgets import QMainWindow, QApplication, QMessageBox, QFileDialog
+# Import gui py file created by QT Designer
+from ui_main_window import Ui_MainWindow
+# Import controller class that does all the work
+from apod_class import APODClass
+# Import dialog boxes that display the photos
+from apod_photo_dialog import PhotoDialog
+from apod_hd_photo_dialog import HdPhotoDialog
+
+
+class APODViewer(QMainWindow, Ui_MainWindow):
+
+    def __init__(self):
+        super(APODViewer, self).__init__()
+        """NASA APOD Viewer."""
+        # Create an instance of the photo dialog gui's
+        # Used to ashow APOD photos
+        self.photo_dialog = PhotoDialog()
+        self.photo_hd_dialog = HdPhotoDialog()
+
+        # Create apod_class object to access methods and properties
+        # APODClass does all the work on getting APOD data
+        self.apod_class = APODClass()
+        # Create the GUI from Ui_MainWindow argument
+        self.setupUi(self)
+        # Remove title bar
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        # Set window title bar icon, shows in task bar
+        my_icon = QIcon()
+        my_icon.addFile("telescope-icon-24300")
+        self.setWindowIcon(my_icon)
+
+        # Get screen object
+        screen = QApplication.primaryScreen()
+        # Get screen size
+        size = screen.size()
+        # Get screen width and height
+        self.screen_width = size.width()
+        self.screen_height = size.height()
+        # print(f"{self.screen_width} x {self.screen_height}")
+
+        # Get today's date
+        display_date = datetime.date.today()
+        # Set dateEdit widget to today's date
+        self.dateEdit.setDate(display_date)
+
+        # Connect the clicked event/signal to the display_data handler/slot
+        # Display APOD with button or return/enter keys
+        self.btn_display_apod.clicked.connect(self.display_apod_data)
+        # Set shortcut keys for display APOD buttons
+        self.btn_display_apod.setShortcut("Return")
+        self.btn_display_apod.setShortcut("Enter")
+
+        # Connect signal to display_random_apod photo handler/slot
+        self.btn_random_photo.clicked.connect(self.display_random_apod)
+
+        # Make thumbnail label clickable by
+        # assiging a method to the mouseReleaseEvent
+        self.lbl_thumbnail.mouseReleaseEvent = self.display_photo
+
+        # Connect buttons to display photo methods
+        self.btn_full_photo.clicked.connect(self.display_photo)
+        self.btn_hd_photo.clicked.connect(self.display_hd_photo)
+
+        self.btn_save_file.clicked.connect(self.save_photo)
+
+        # Exit the program with button or escape key
+        self.btn_exit.clicked.connect(self.close)
+        self.btn_exit.setShortcut("Esc")
+
+        # Display initial NASA APOD on startup
+        # Create QTimer to allow GUI a chance to start
+        timer = QTimer()
+        # After GUI has started, display apod data
+        # 500 ms delay before APOD photo is displayed
+        timer.singleShot(500, self.display_apod_data)
+
+# --------------------- DISPLAY APOD ---------------------------------------#
+    def display_photo(self, *args):
+        """Display APOD in dialog box."""
+        # Set APOD photo Pixmap to label
+        self.photo_dialog.photo_label.setPixmap(self.apod_class.img)
+        # Display APOD photo dialog box
+        self.photo_dialog.display_info()
+
+# --------------------- DISPLAY HD APOD ------------------------------------#
+    def display_hd_photo(self, *args):
+        """Display HD APOD in dialog box."""
+        height = self.apod_class.hd_img.height()
+        # If the height of the image is greater that the screen height
+        # downsize image to fit the screen height - 40 pixels
+        if self.screen_height < height:
+            image = self.apod_class.hd_img.scaledToHeight(
+                self.screen_height - 40
+            )
+            self.photo_hd_dialog.photo_hd_label.setPixmap(image)
+        else:
+            # Set APOD photo to label at normal size
+            self.photo_hd_dialog.photo_hd_label.setPixmap(
+                self.apod_class.hd_img)
+
+        # Display APOD photo dialog box
+        self.photo_hd_dialog.display_info()
+
+# --------------------- DISPLAY APOD DATA ----------------------------------#
+    def display_apod_data(self):
+        """Get and display APOD description and thumbnail on form label."""
+        # Disable buttons while updating apod data
+        self.disable_buttons()
+        # Process UI events to show new button state
+        QApplication.processEvents()
+        # Get date from dateEdit widget
+        temp_date = self.dateEdit.date()
+        # Convert QDate to Python date
+        display_date = temp_date.toPython()
+
+        # Set busy cursor
+        QGuiApplication.setOverrideCursor(PySide6.QtGui.QCursor(Qt.WaitCursor))
+        # Get display_date's APOD data
+        self.apod_class.get_data(display_date)
+        # Reset cursor
+        QGuiApplication.restoreOverrideCursor()
+
+        # Display explanation in label
+        self.lbl_explanation.setText(
+            f"{self.apod_class.explanation}")
+
+        if self.apod_class.img == None:
+            # If there is not an image, it is a video
+            self.lbl_thumbnail.setText("YouTube Video")
+            self.btn_full_photo.setEnabled(False)
+
+        else:
+            # Set APOD photo to image on label
+            self.lbl_thumbnail.setPixmap(self.apod_class.img)
+            # Scale image to fit label for thumbnail
+            self.lbl_thumbnail.setScaledContents(True)
+            # Set label size
+            self.lbl_thumbnail.resize(225, 225)
+
+        # Enable button when done updating apod data
+        self.enable_buttons()
+        # Process UI events to show new button state
+        QApplication.processEvents()
+
+# --------------------- DISPLAY RANDOM APOD DATA ---------------------------#
+    def display_random_apod(self):
+        """Get and display APOD description and thumbnail on form label."""
+        # Disable button while updating apod data
+        self.disable_buttons()
+
+        # Process UI events to show new button state
+        QApplication.processEvents()
+
+        # Set busy cursor
+        QGuiApplication.setOverrideCursor(PySide6.QtGui.QCursor(Qt.WaitCursor))
+        self.apod_class.get_random_photo()
+        # Reset cursor
+        QGuiApplication.restoreOverrideCursor()
+        
+        # Display explanation in label
+        self.lbl_explanation.setText(
+            f"{self.apod_class.explanation}")
+        # Convert APOD date to QDate format
+        date = QDate.fromString(self.apod_class.date, "yyyy-MM-dd")
+        # Display QDate in DateEdit
+        self.dateEdit.setDate(date)
+        if self.apod_class.img == None:
+            # If there is not an image, it is a video
+            self.lbl_thumbnail.setText("YouTube Video")
+            self.btn_full_photo.setEnabled(False)
+        else:
+            self.btn_full_photo.setEnabled(True)
+            # Set APOD photo to image on label
+            self.lbl_thumbnail.setPixmap(self.apod_class.img)
+            # Scale image to fit label
+            self.lbl_thumbnail.setScaledContents(True)
+            # Set label size
+            self.lbl_thumbnail.resize(225, 225)
+
+        # Enable button when done updating apod data
+        self.enable_buttons()
+        # Process UI events to show new button state
+        QApplication.processEvents()
+
+# ----------------------------- SAVE PHOTO ---------------------------------#
+    def save_photo(self):
+        """Save the image to file."""
+        # Convert jpg to PySide6 pixmap format
+        pixmap = QPixmap(self.apod_class.hd_img)
+        # Prompt the user for the filename
+        image_file, _ = QFileDialog.getSaveFileName(
+            self, "Save Image",
+            "",
+            "JPG (*.jpg)")
+        # If the filename and the image exists, save image
+        if image_file and self.apod_class.hd_img != None:
+            # Save the file using Pixmap.save method
+            pixmap.save(image_file)
+            QMessageBox.information(
+                self, "Success",
+                "Image saved.", QMessageBox.Ok
+            )
+        else:
+            # Show user the image was not saved
+            QMessageBox.information(
+                self, "Error",
+                "Unable to save image.", QMessageBox.Ok
+            )
+
+# --------------------- ENABLE BUTTONS -------------------------------------#
+    def enable_buttons(self):
+        self.btn_display_apod.setEnabled(True)
+        self.btn_full_photo.setEnabled(True)
+        self.btn_hd_photo.setEnabled(True)
+        self.btn_random_photo.setEnabled(True)
+        self.btn_save_file.setEnabled(True)
+        self.btn_exit.setEnabled(True)
+
+# --------------------- DISABLE BUTTONS ------------------------------------#
+    def disable_buttons(self):
+        self.btn_display_apod.setEnabled(False)
+        self.btn_full_photo.setEnabled(False)
+        self.btn_hd_photo.setEnabled(False)
+        self.btn_random_photo.setEnabled(False)
+        self.btn_save_file.setEnabled(False)
+        self.btn_exit.setEnabled(False)
+
+# ----------- OVERRIDE MOUSE EVENTS TO MOVE PROGRAM WINDOW -----------------#
+    def mousePressEvent(self, event):
+        """Override the mousePressEvent."""
+        # Store the current position of the mouse in previous position
+        self.previous_pos = event.globalPosition()
+
+    def mouseMoveEvent(self, event):
+        """Override the mouseMoveEvent."""
+        # Subtract the previous position from the current position
+        delta = event.globalPosition() - self.previous_pos
+        # Add the delta calculation to the current position
+        self.move(self.x() + delta.x(), self.y()+delta.y())
+        # Store the current position
+        self.previous_pos = event.globalPosition()
+        # self._drag_active = True
+
+    def mouseReleaseEvent(self, event):
+        """Override the mouseReleaseEvent."""
+        pass
+
+
+def main():
+    # -------------------------- START APPLICATION -------------------------#
+    # Create application object
+    apod_viewer = QApplication(sys.argv)
+    # Set a QT style
+    apod_viewer.setStyle("Fusion")
+    # Create program object
+    window = APODViewer()
+    # Make program visible
+    window.show()
+    # Execute the program, setup clean exit of program
+    sys.exit(apod_viewer.exec())
+
+
+main()
---
+++ README.md
@@ -0,0 +1,49 @@
+# NASA APOD App in Python Pyside6
+## Project Description
+NASA Astronomy Photo of the Day
+
+Example project for Intro to Computer Science 1 Think Aloud Individual Project.
+
+## Requirements
+ Python 3.11
+ 
+ PySide 6.4.0.1
+ - pip install pyside6
+ - pip install requests
+
+## Changes
+- 12-17-22: Add busy cursor when loading images from url
+- 12-06-22: Add Save File button to save hd image to file
+- 10-30-22: Resize HD photo to fit vertical size of screen
+- 10-23-22: Add random photo button
+- 10-15-22: Add QTimer to allow GUI to show before getting initial APOD
+- 10-15-22: Add tooltips
+- 10-15-22: Click thumbnail to display full photo
+- 10-15-22: Display hd photo
+- 10-08-22: Display full photo
+- 10-07-22: Show thumbnail of current day's APOD
+- 10-08-22: Display image or YouTube video
+- 09-29-22: Calendar widget chooses that days APOD
+- 09-29-22: Get and display current day's APOD description
+- 09-20-22: Convert QT Designer file main_window.ui to main_ui.py
+- 09-20-22: Design UI in QTDesigner 
+
+## Version History
+- (10-23-22) V4 Add retrieve random photo.
+- (10-08-22) V3 Retrieve and display full and hd photo.
+- (10-07-22) V2 Retrieve and display thumbnail.
+- (09-27-22) V1 Connect to NASA APOD API. Retrieve and display current APOD description.
+
+## UI Design
+V3 GUI
+
+![](/images/gui_design_3.png)
+
+V1 GUI
+
+![](/images/gui_design_1.png)
+
+### License
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
+
+Copyright (c) 2023 William A Loring
